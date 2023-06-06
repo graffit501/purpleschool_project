116c87d3a0be6e9ea0ce7a4922ae414f6c031275
Sergey <canik3669@gmail.com>
2023-06-06 13:00:49 +0300

    Четвертая ступень - упражнение расчет проекта
    работа с переменными и присвоением

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello, world')
+/*
+Ваша часовая ставка 80$ и вы готовы работать не более 5 часов в день 5 дней в неделю (кроме выходных).
+К вам приходит заказчик и предлагает заказ на 40 часов работы. Сейчас понедельник. Вы должны уехать через 11 дней.
+Выведете в консоль:
+Boolean переменную успеете ли вы взяться за работу
+Сколько вы за неё попросите?
+*/
+
+const stake = 80;
+const workday = 5;
+const workingHour = 5;
+const contract =  40;
+let payment = contract*80;
+let result = contract/workingHour>5?contract/workingHour+2:contract/workingHour;
+console.log(`Смогу лия выполнить ${result<11}`);
+console.log (`ставка ${payment}$`);
