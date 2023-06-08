44a4fb2457273ae87f63a7a294dc54276bd6ceba
Sergey <canik3669@gmail.com>
2023-06-08 10:59:20 +0300

    6 state
    булевая логика оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 /*
-Методом prompt получите ответ пользователя на вопрос 
-"Сколько будет 7 + или - 15?". Если ответ верен, выведите 
-в консоли "Успех", если нет - "Вы робот!", а если он введёт 
-"Я не робот", то тоже "Успех".
+Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
+Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
+Он не забанен (isBanned)
+Игра не куплена (isExist)
+Игра в продаже (isSelling)
+Напишите условие для покупки и выведите в консоль результат.
 */
-const inputString = prompt('Сколько будет 7 + или - 15?');
-/*
-if(!isNaN(Number(inputString))){
-switch(Number(inputString)){
-    case 22:
-    case -8:
-        console.log('Успех!');
-        break;
-    default:
-        console.log('"Вы робот!"');
-    }
-}else{
-    if(inputString === 'Я не робот'){
-        console.log('Успех!');
-    }else{
-        console.log('"Вы робот!"');
-    }
-}
-*/
-switch(true){
-    case inputString === 'Я не робот':
-    case Number(inputString) === 22:
-    case Number(inputString) === -8:
-        console.log('Успех!');
-        break;
-    default:
-        console.log('"Вы робот!"');   
-
-}
-
+let balance =10;
+let bonusBalance =30;
+let isBanned = false;
+let isExist = false;
+let isSelling = true;
+console.log((balance>1000 || bonusBalance>100)&& !isBanned && !isExist && isSelling)
