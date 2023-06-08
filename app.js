0a49cae3a53b363fd33d9ac43d2e19273401aeae
Sergey <canik3669@gmail.com>
2023-06-08 19:33:30 +0300

    7 stage
    работа с функциями выполнение заданий

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 /*
-Пользователь хочет приобрести игру в магазине. Он может это сделать только если:
-Eго баланс больше 1000 (balance) или число бонусов больше 100 (bonusBalance)
-Он не забанен (isBanned)
-Игра не куплена (isExist)
-Игра в продаже (isSelling)
-Напишите условие для покупки и выведите в консоль результат.
-*/
-let balance =10;
-let bonusBalance =30;
-let isBanned = false;
-let isExist = false;
-let isSelling = true;
-console.log((balance>1000 || bonusBalance>100)&& !isBanned && !isExist && isSelling)
+Пользователь:
+Возраст
+Наличие работы
+Деньги
+Нужно проверить может ли он купить новый MacBook за 2000$?
+ Он может брать не только свои деньги, но и взять кредит. 
+ Ему дадут 500$, только если ему больше 24-х лет и он имеет работу, 100$ если ему просто больше 24-х лет и 0 в ином случае.
+  Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false*/
+
+
+function issuanceOfCredit(age,job){
+    switch(true){
+        case age>=24 && job:
+            return 500;
+        case age>=24 && !job:
+            return 100;
+        default:
+            return 0;
+    }
+}
+function buyingLaptop(prodctPrice,age,job,money){
+    if(money>= prodctPrice){
+        return true
+    }else{
+        const creditResult = issuanceOfCredit(age,job);
+        return Number(money)+Number(creditResult) >= prodctPrice;
+    }
+}
+console.log(buyingLaptop(2000,24,false,1900));
