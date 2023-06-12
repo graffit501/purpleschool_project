02c30de310ea7f229c0856854c1e157e5d1d5290
Sergey <canik3669@gmail.com>
2023-06-12 20:18:37 +0300

    9 stage
    замыкание с использованием стрелочной функции

--- app.js
+++ app.js
@@ -9,3 +9,8 @@ function power(pow){
 }
 const powerOfTwo = power(2)// присваиваем перемной функцию 
 console.log(powerOfTwo(10));// так как функция возвращает функцию при обращение к ней мы получаем необходимое нам значение
+
+
+/// реалиция через стрелочную функцию 
+const power2 = pow=>num=> num ** pow
+console.log(power2(3))
