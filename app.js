c2f8d96fa029c8affb468b9348d9695b01ba91ad
Sergey <canik3669@gmail.com>
2023-06-12 20:07:14 +0300

    Возврат функции, основы замыкания

--- app.js
+++ app.js
@@ -1,57 +1,11 @@
 /*
-Есть выгрузка операций пользователя
-const operations = [1000, -700, 300, -500, 10000];
-а так же начальный баланс в 100$
-Необходимо сделать функции расчета:
-- Итогового баланса
-- Наличия отрицательного баланса (если после очередной операции баланс < 0,
-     то выдавать false)
-- Расчета среднего расхода и среднего дохода
-*/
-let balance =  100;
-const operations = [1000, -700, 300, -500, 10000];
+концепция замыкания
+ */
 
-function totalBalance(balance,operArr){
-    for(const item of operArr){
-        balance+=item;
+function power(pow){
+    return function(num){
+        return num ** pow
     }
-    return balance;
 }
-console.log(totalBalance(balance,operations));
-
-function checkBalance(balance,operations){
-    let i =0;
-    let result = true;
-    for(const item of operations){
-        balance+=item;
-        if(balance<0){
-            //console.log(` У вас отрицательный баланс дальнешие операции не возможны`);
-            result =  false;
-            break;
-        }
-    }
-   console.log(result?'у вас положительный баланс вы можете продолжить покупки':' У вас отрицательный баланс дальнешие операции не возможны')
-   return result;
-}
-checkBalance(balance,operations);
- 
-
-function averageValueBalance(balance, operations){
-    let i = 0;
-    let j = 0;
-    let receipt = 0;
-    let cost = 0;
-    for(const item of operations){
-        if(item>0){
-            i++;
-            receipt+=item;
-        }
-        else{
-            j++;
-            cost+=item*-1;
-        }
-    }
-    console.log( `среднее поступление на счет составили ${receipt/i}`);
-    console.log(`средний расход составил ${cost/j}`);
-}
-averageValueBalance(balance,operations);
+const powerOfTwo = power(2)// присваиваем перемной функцию 
+console.log(powerOfTwo(10));// так как функция возвращает функцию при обращение к ней мы получаем необходимое нам значение
