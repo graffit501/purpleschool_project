9ac442e57c466e367f2c959ce501b0027a40d944
Sergey <canik3669@gmail.com>
2023-06-12 19:13:30 +0300

    9 stage
    работат с циклами,  задание управление балансом

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
 /*
-Дан произвольный url вида - https://purpleschool.ru/course/javascript
-Нужно сделать функцию, которая выводит в консоль:
-Протокол (https)
-Доменное имя (purpleschool.ru)
-Путь внутри сайта (/course/javascript)
- 
+Есть выгрузка операций пользователя
+const operations = [1000, -700, 300, -500, 10000];
+а так же начальный баланс в 100$
+Необходимо сделать функции расчета:
+- Итогового баланса
+- Наличия отрицательного баланса (если после очередной операции баланс < 0,
+     то выдавать false)
+- Расчета среднего расхода и среднего дохода
 */
+let balance =  100;
+const operations = [1000, -700, 300, -500, 10000];
 
-
-
-const  url = 'https://purpleschool.ru/course/javascript';
-const urlArr = url.split('/');
-//console.log(urlArr);
-getUrl(url);
-
-function getUrl(url){
-    const [protocol,_,name,...other] = url.split('/');
-    if(protocol !=='https:'){
-        return;
+function totalBalance(balance,operArr){
+    for(const item of operArr){
+        balance+=item;
     }
-    console.log(protocol.slice(0,-1));
-    console.log(name);
-    console.log(other.join('/'))
-
+    return balance;
 }
+console.log(totalBalance(balance,operations));
+
+function checkBalance(balance,operations){
+    let i =0;
+    let result = true;
+    for(const item of operations){
+        balance+=item;
+        if(balance<0){
+            //console.log(` У вас отрицательный баланс дальнешие операции не возможны`);
+            result =  false;
+            break;
+        }
+    }
+   console.log(result?'у вас положительный баланс вы можете продолжить покупки':' У вас отрицательный баланс дальнешие операции не возможны')
+   return result;
+}
+checkBalance(balance,operations);
+ 
+
+function averageValueBalance(balance, operations){
+    let i = 0;
+    let j = 0;
+    let receipt = 0;
+    let cost = 0;
+    for(const item of operations){
+        if(item>0){
+            i++;
+            receipt+=item;
+        }
+        else{
+            j++;
+            cost+=item*-1;
+        }
+    }
+    console.log( `среднее поступление на счет составили ${receipt/i}`);
+    console.log(`средний расход составил ${cost/j}`);
+}
+averageValueBalance(balance,operations);
