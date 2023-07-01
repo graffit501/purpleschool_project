35a6499f91b3a90604ac59e2764266c99131d0e0
Sergey <canik3669@gmail.com>
2023-07-01 10:15:15 +0300

    12 stage
    упражнение проверки телефона

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const str = "Вася aka Terminator Perdinator Пупкин";
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = ' +7(910) 323-53-56 ';
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
 
 
-console.log(`${str.slice(0,str.indexOf('я')+1)} ${str.slice(str.indexOf("П"),str.length)}`)
+function isNumber(number){
+    let formstring = number.replace("+","");
+    formstring = formstring.replace("(","");
+    formstring = formstring.replace(")","");
+    formstring = formstring.replace("+7","8");
+    formstring = formstring.replaceAll(" ","");
+    formstring = formstring.replaceAll("-","");
+    if(formstring.length != 11){
+        return false
+    }
+    if(isNaN(formstring)){
+        return false
+    }
+    if(!formstring.startsWith("8")){
+        return false
+    }    
+    return true
+}
+console.log(isNumber(num3Error));
