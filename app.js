7e95310164a9796a88a60bc4491938afcb3d6ca8
Sergey <canik3669@gmail.com>
2023-08-13 21:33:21 +0300

    17 stage local storage

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 'use strict'
 function submitForm(){
     const input  = document.querySelector('.input').value;
+    let keynumber  = Number(localStorage.getItem('keynumber'));
+    if(!keynumber){
+        localStorage.setItem('keynumber',1)
+        keynumber = 1;    
+    }
     if(!input){
         return
     }
     document.querySelector(".panel").innerText = input;
     document.querySelector('.input').value = "";
+    localStorage.setItem(keynumber,input);
+    console.log(keynumber)
+    keynumber++;
+    localStorage.setItem('keynumber', keynumber)
+
 }
 function inputChange(e){
     if(e.code == 'Enter'){
         submitForm()
     }
 }
-function gettag(){
-    
+function gettag(){    
     document.querySelectorAll('div.one').forEach((element)=>{console.log(element.innerText)})
     getselector("div#two");
-    getselector('span[user-id = "4"]')
-    
+    getselector('span[user-id = "4"]');
+     
 }
 function getselector(selector){
     console.log( document.querySelector(selector).innerText);
